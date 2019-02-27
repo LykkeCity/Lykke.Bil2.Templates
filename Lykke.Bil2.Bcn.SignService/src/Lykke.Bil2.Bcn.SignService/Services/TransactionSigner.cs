a53dfe66d333ef54f2beb2f13f007a6a3cbd6af0
Konstantin Ryazantsev <kj.ryazancev@gmail.com>
2019-02-27 09:16:55 +0300

    Update Lykke.Bil2.Bcn.SignService/src/Lykke.Bil2.Bcn.SignService/Services/TransactionSigner.cs
    Co-Authored-By: cassade <cassade@users.noreply.github.com>

--- Lykke.Bil2.Bcn.SignService/src/Lykke.Bil2.Bcn.SignService/Services/TransactionSigner.cs
+++ Lykke.Bil2.Bcn.SignService/src/Lykke.Bil2.Bcn.SignService/Services/TransactionSigner.cs
@@ -9,7 +9,7 @@ namespace Lykke.Bil2.Bcn.SignService.Services
 {
     public class TransactionSigner : ITransactionSigner
     {
-        public TransactionSigner(/* TODO: Provide specific settings and dependecies, if necessary */)
+        public TransactionSigner(/* TODO: Provide specific settings and dependencies, if necessary */)
         {
         }
 
